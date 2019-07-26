e37c98286c7a097a81860a346e3fb591e5e26752
Andres <arocha.p43@gmail.com>
2019-07-26 13:21:28 -0400

    Finally show images

--- src/Screens/Home/HomeScreen.js
+++ src/Screens/Home/HomeScreen.js
@@ -1,6 +1,7 @@
 import React, { Component } from "react";
 import Unsplash from 'unsplash-js/native';
-import { Platform, StyleSheet, Text, View } from "react-native";
+import { Platform, StyleSheet, Text, View, FlatList, Image, TouchableOpacity } from "react-native";
+import PhotoGrid from 'react-native-image-grid';
 
 const instructions = Platform.select({
   ios: "Press Cmd+R to reload,\n" + "Cmd+D or shake for dev menu",
@@ -13,25 +14,85 @@ const unsplash = new Unsplash({
   secret: "a5ab4ed2efdc772dca8d5636a26c0d897907df38cd92baa9067e57093d9596b5"
 });
 export default class App extends Component {
+
+  constructor() {
+    super();
+    state = {
+      images: null,
+      images_test:[],
+    };
+  }
+
+
   componentWillMount() {
-    unsplash.photos.getPhoto("pFqrYbhIAXs")
+    unsplash.users.profile("corneschi")
     .then(json => {
-      const parsedResponse = JSON.parse(json._bodyInit);
-      console.log(parsedResponse);
+      let profile = JSON.parse(json._bodyInit);
+      console.log("Images", profile.photos, profile.photos[0], profile.photos[1]);
+        this.setState({
+          images: profile.photos,
+        });
       // Your code
     })
   .catch(err => {
     console.log("ERROR", err)
     // Your flawless error handling code
   });
+  let items = Array.apply(null, Array(60)).map((v, i) => {
+    //Using demo placeholder images but you can add your images here
+    return { id: i, urls : {small: 'http://placehold.it/200x200?text=' + (i +1)} };
+  });
+  this.setState({ images_test: items });
+
+  }
+
+  renderHeader() {
+    //Header of the Screen
+    return <Text style={{padding:16, fontSize:20, color:'white', backgroundColor:'green'}}>
+               Image Gallery
+           </Text>;
+  }
+
+  renderItem(item, itemSize, itemPaddingHorizontal) {
+    //Single item of Grid
+    console.log("ITEM", item);
+    return (
+      <TouchableOpacity
+        key={item.id}
+        style={{
+          width: itemSize,
+          height: itemSize,
+          paddingHorizontal: itemPaddingHorizontal,
+        }}
+        onPress={() => {
+        }}>
+        <Image
+          resizeMode="cover"
+          style={{ flex: 1 }}
+          source={{ uri: item.urls.small }}
+        />
+      </TouchableOpacity>
+    );
   }
 
   render() {
+
     return (
       <View style={styles.container}>
         <Text style={styles.welcome}>Welcome to React Native!</Text>
         <Text style={styles.instructions}>To get started, edit App.js</Text>
         <Text style={styles.instructions}>{instructions}</Text>
+        <View style={styles.containerStyle}>
+      <PhotoGrid
+          data={this.state.images? this.state.images :  this.state.images_test}
+          itemsPerRow={3}
+          //You can decide the item per row
+          itemMargin={1}
+          itemPaddingHorizontal={1}
+          renderHeader={this.renderHeader}
+          renderItem={this.renderItem.bind(this)}
+        /> 
+        </View>
       </View>
     );
   }
@@ -53,5 +114,40 @@ const styles = StyleSheet.create({
     textAlign: "center",
     color: "#333333",
     marginBottom: 5
-  }
+  },    
+  MainContainer: {
+      justifyContent: 'center',
+      flex: 1,
+      paddingTop: 30,
+    },
+    imageThumbnail: {
+      justifyContent: 'center',
+      alignItems: 'center',
+      height: 100,
+    },
+    containerStyle: {
+      justifyContent: 'center',
+      flex: 1,
+      marginTop: 20,
+    },
+    fullImageStyle: {
+      justifyContent: 'center',
+      alignItems: 'center',
+      height: '100%',
+      width: '98%',
+      resizeMode: 'contain',
+    },
+    modelStyle: {
+      flex: 1,
+      justifyContent: 'center',
+      alignItems: 'center',
+      backgroundColor: 'rgba(0,0,0,0.4)',
+    },
+    closeButtonStyle: {
+      width: 25,
+      height: 25,
+      top: 9,
+      right: 9,
+      position: 'absolute',
+    },
 });
